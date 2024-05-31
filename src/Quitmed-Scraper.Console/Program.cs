08c8f677a817d5f25cc3a7c85ebd5dcd5a06b8ed
Jyhe <jyhe.mark@hurtigruten.com>
2024-05-31 22:39:14 +1000

    reinstated test data

--- src/Quitmed-Scraper.Console/Program.cs
+++ src/Quitmed-Scraper.Console/Program.cs
@@ -2,10 +2,12 @@
 using System.Text.RegularExpressions;
 using Quitmed_Scraper.Console.QuitMed;
 
-var httpClient = new HttpClient();
-httpClient.DefaultRequestHeaders.UserAgent.ParseAdd("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3");
-var response = await httpClient.GetAsync("https://quitmed.com.au/collections/all");
-var responseBody = await response.Content.ReadAsStringAsync();
+// var httpClient = new HttpClient();
+// httpClient.DefaultRequestHeaders.UserAgent.ParseAdd("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3");
+// var response = await httpClient.GetAsync("https://quitmed.com.au/collections/all");
+// var responseBody = await response.Content.ReadAsStringAsync();
+
+var responseBody = File.ReadAllText(@"D:\Jyhe\Downloads\sample-data.html");
 
 var metaData = Regex.Match(responseBody, @"var meta = (.*?);", RegexOptions.Singleline);
 
