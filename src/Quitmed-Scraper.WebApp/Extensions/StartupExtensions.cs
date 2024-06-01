05b6d9528552f5cfbe65be3ba53d1450aefdf128
Jyhe <jyhe.mark@hurtigruten.com>
2024-06-01 15:48:53 +1000

    Added HTTPClient to webapp

--- src/Quitmed-Scraper.WebApp/Extensions/StartupExtensions.cs
+++ src/Quitmed-Scraper.WebApp/Extensions/StartupExtensions.cs
@@ -12,6 +12,10 @@ internal static class StartupExtensions
     {
         BindConfigurationModels(services, configuration);
 
+        services.AddHttpClient<HttpClient>(client =>
+        {
+            client.DefaultRequestHeaders.UserAgent.ParseAdd("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3");
+        });
         services.AddDbContext<QuitmedScraperDatabaseContext>();
         services.AddAkka("quitmed-scraper-actor-system", builder =>
         {
