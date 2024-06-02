ab87623a477dd45ee60d3132f81601a3f2240cb9
Jyhe <jyhe.mark@hurtigruten.com>
2024-06-02 16:26:40 +1000

    Split out home, dispensary and products page

--- src/Quitmed-Scraper.WebApp/Extensions/StartupExtensions.cs
+++ src/Quitmed-Scraper.WebApp/Extensions/StartupExtensions.cs
@@ -1,4 +1,5 @@
 ﻿using Akka.Hosting;
+using MudBlazor.Services;
 using Quitmed_scraper.Database;
 using Quitmed_scraper.Database.Configuration;
 using Quitmed_Scraper.Library.Actors;
@@ -12,10 +13,10 @@ internal static class StartupExtensions
     {
         BindConfigurationModels(services, configuration);
 
-        services.AddHttpClient<HttpClient>(client =>
-        {
-            client.DefaultRequestHeaders.UserAgent.ParseAdd("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3");
-        });
+        var httpClient = new HttpClient();
+        httpClient.DefaultRequestHeaders.UserAgent.ParseAdd("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3");
+
+        services.AddSingleton(httpClient);
         services.AddDbContext<QuitmedScraperDatabaseContext>();
         services.AddAkka("quitmed-scraper-actor-system", builder =>
         {
@@ -26,6 +27,7 @@ internal static class StartupExtensions
                     system.ActorOf(dependencies.Props<OrchestrationActor>(), "scraper-orchestration-actor");
                 });
         });
+        services.AddMudServices();
     }
 
     private static void BindConfigurationModels(IServiceCollection services, IConfiguration configuration)
---
+++ src/Quitmed-Scraper.WebApp/PriceHelper.cs
@@ -0,0 +1,14 @@
+﻿namespace Quitmed_Scraper.WebApp;
+
+internal static class PriceHelper
+{
+    public static string FormatAsPrice(int price)
+    {
+        return (price / 100.0m).ToString("C");
+    }
+    
+    public static double FormatAsPriceDouble(int price)
+    {
+        return (double)price / 100.0;
+    }
+}
